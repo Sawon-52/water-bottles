3305c4674c954d74d6c3ad97bbb8d905f59b1787
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-03-24 15:01:58 +0600

    set data to local storage and get these data using id

--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Bottle } from "../Bottle/Bottle";
 import "./Bottles.css";
 import { Cart } from "../Cart/Cart";
+import { addToLS, getLocalStorage } from "../Localstorage/Localstorage";
 
 export const Bottles = () => {
   const [bottles, setBottles] = useState([]);
@@ -19,8 +20,17 @@ export const Bottles = () => {
   function handleAddtoCart(bottle) {
     const newCart = [...cart, bottle];
     setCart(newCart);
+    addToLS(bottle.id);
   }
 
+
+  useEffect(() => {
+    if (bottles.length > 0) {
+      const storedCart = getLocalStorage();
+      console.log(storedCart);
+    }
+  }, [bottles]);
+  
   return (
     <div className="Bottls">
       <h1>Awosome Bottles Are Available </h1>
